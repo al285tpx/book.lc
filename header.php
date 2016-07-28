66fc9aa9d88e520704bf2b335e54936f225e896e
al285tpx <AlexZaikov@ya.ru>
2016-07-28 22:47:23 +0500

    header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <meta charset="utf-8">
+        <title>Books</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    </head>
+    <body>
+    <div class="container">
+        <!-- Header (navbar) -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <h3><a href="index.php?option">Books</a></h3>
+                </div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php?option=add">Создать запись</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- END Header (navbar) -->
+
