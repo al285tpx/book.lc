9a10fad1796ec563c19c59b9c2f67466f5318e56
Зайков Александр Сергеевич <zaykov.aleksandr@itmh.ru>
2016-09-24 21:53:41 +0500

    Переход на mysqli

--- classes/main.php
+++ classes/main.php
@@ -1,23 +1,19 @@
 <?php
 
-/**
- * Description of main
- *
- * @author Пользователь
- */
 class main extends ACore {
     
     public function get_content() {
         
         $query = "SELECT id, title, year, author, col_str FROM d_books";
-        $result = mysql_query($query);
+        $result = mysqli_query($this->db, $query);
         if (!$result) {
-            exit(mysql_error());
+            exit(mysqli_error($this->db));
         }
         echo "<div id='main'>";
         //echo "<a style='color:red' href='?option=add'>Добавить новую запись</a><hr>";
         echo "<p style='font-size:14px;'><table id='admin_table' class='table'>
             <tr>
+                <th>Checkbox<th>
                 <th>Title</th>
                 <th>Year</th>
                 <th>Author</th>
@@ -31,8 +27,8 @@ class main extends ACore {
         }
         
         $row = array();
-        for ($i=0; $i < mysql_num_rows($result);$i++) { //цикл будет вытаскивать содержимое БД пока $i не станет меньше количества строк в $result 
-          $row = mysql_fetch_array($result, MYSQL_ASSOC);
+        for ($i=0; $i < mysqli_num_rows($result);$i++) { //цикл будет вытаскивать содержимое БД пока $i не станет меньше количества строк в $result 
+          $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
           printf("<p><tr><th>
               <a style='color:#585858' href='?option=update&id_text=%s'>%s</a></th> 
               <th>%s</th> 
