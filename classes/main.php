0f7a6cfdfc1146ab1b91e0c1eea257be7fa00cc4
Зайков Александр Сергеевич <zaykov.aleksandr@itmh.ru>
2016-09-24 22:06:30 +0500

    добавил чекбоксы для пакетного удаления

--- classes/main.php
+++ classes/main.php
@@ -29,12 +29,13 @@ class main extends ACore {
         $row = array();
         for ($i=0; $i < mysqli_num_rows($result);$i++) { //цикл будет вытаскивать содержимое БД пока $i не станет меньше количества строк в $result 
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-          printf("<p><tr><th>
-              <a style='color:#585858' href='?option=update&id_text=%s'>%s</a></th> 
+          printf("<p><tr>
+              <th><input type='checkbox' name='item[]' value= . '%s'. /></th>
+              <th><a style='color:#585858' href='?option=update&id_text=%s'>%s</a></th> 
               <th>%s</th> 
               <th>%s</th> <th>%s |
               <a style='color:#red' href='?option=delete&del=%s'>Удалить</a></th>
-              </p>", $row['id'], $row['title'],$row['year'], $row['author'], $row['col_str'], $row['id']);
+              </p>", $row['id'], $row['id'], $row['title'],$row['year'], $row['author'], $row['col_str'], $row['id']);
         }
         
         
