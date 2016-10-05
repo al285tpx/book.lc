59c0c2328401e5a869208366a266f527b4974517
Зайков Александр Сергеевич <zaykov.aleksandr@itmh.ru>
2016-10-05 21:15:22 +0500

    подключение библиотеки jquery

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Books</title>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     </head>
