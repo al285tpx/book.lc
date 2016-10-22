417c0731035ee25527a03a2880984ad357fabf61
Зайков Александр Сергеевич <zaykov.aleksandr@itmh.ru>
2016-10-23 00:34:12 +0500

    comment

--- classes/main.php
+++ classes/main.php
@@ -25,19 +25,20 @@ class main extends ACore {
             echo $_SESSION['res'];
             unset($_SESSION['res']);
         }
-        
-        $row = array();
-        for ($i=0; $i < mysqli_num_rows($result);$i++) { //цикл будет вытаскивать содержимое БД пока $i не станет меньше количества строк в $result 
-          $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-          printf("<p><tr>
-              <th><input type='checkbox' name='item[]' value= '.%s.' /></th>
-              <th><a style='color:#585858' href='?option=update&id_text=%s'>%s</a></th> 
-              <th>%s</th> 
-              <th>%s</th> <th>%s |
-              <a style='color:#red' href='?option=delete&del=%s'>Удалить</a></th>
-              </p>", $row['id'], $row['id'], $row['title'],$row['year'], $row['author'], $row['col_str'], $row['id']);
-        }
-        
+        //здесь вместо FOR будет While который будет передовать данные классу groupdel
+        //через GET
+        /**$row = array();
+        *for ($i=0; $i < mysqli_num_rows($result);$i++) { //цикл будет вытаскивать содержимое БД пока $i не станет меньше количества строк в $result 
+        *  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        *  printf("<p><tr>
+        *      <th><input type='checkbox' name='item[]' value= '.%s.' /></th>
+        *      <th><a style='color:#585858' href='?option=update&id_text=%s'>%s</a></th> 
+        *      <th>%s</th> 
+        *      <th>%s</th> <th>%s |
+        *      <a style='color:#red' href='?option=delete&del=%s'>Удалить</a></th>
+        *      </p>", $row['id'], $row['id'], $row['title'],$row['year'], $row['author'], $row['col_str'], $row['id']);
+        *}
+        **/
         
         
         printf( "</div>
